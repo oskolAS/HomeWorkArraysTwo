9224a39dc437d5f7bf51e2f47e57261e503e3448
oskol <oskols@mail.ru>
2022-06-05 22:13:11 +0300

    Домашнее задание.

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         }
         System.out.println();
 
-        // Задание 1
+        // Задание 1.
         int summaMonth = 0;
         for (int i = 0; i < arr.length; i++) {
             summaMonth = summaMonth + arr[i];
         }
         System.out.println("Сумма трат за месяц составила " + summaMonth + " рублей");
 
-        // Задание 2
+        // Задание 2.
         int minimumSum = arr[0];
         int maximumSum = arr[0];
         for (final int minimum : arr) {
@@ -29,11 +29,11 @@ public class Main {
         System.out.println("Минимальная сумма трат за день составила " + minimumSum + " рублей");
         System.out.println("Максимальная сумма трат за день составила " + maximumSum + " рублей");
 
-        // Задание 3
+        // Задание 3.
         double averageAmountDay = (double) summaMonth / 30;
         System.out.println("Средняя сумма трат за месяц составила " +averageAmountDay + " рублей");
 
-        //задача 4
+        //задача 4.
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v' , 'I'};
         for (int i = reverseFullName.length-1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
